9925fda97be51049109cf38f3aad65bf16f339de
Raaji-Lamani <165176982+Raaji-Lamani@users.noreply.github.com>
2024-05-20 12:54:21 +0530

    Update v.js

--- v.js
+++ v.js
@@ -4,7 +4,7 @@ const forwardButton = document.getElementById('forward');
 const backwardButton = document.getElementById('backward');
 const videoSource = document.getElementById('videoSource');
 
-const videos = ['Radhamma Radhamma HD video Song _  Nene Raju Nene Mantri _ Rana _ Kajal Agarwal _ Anup Rubens _ Teja.mp4', 'Sukhibhava HD Full Video Song _ NRNM _ Rana Daggubatti _ Kajal Agarwal _ Anup Rubens _ Teja (1).mp4', '15921892-uhd_3840_2160_50fps (1) (1).mp4'];
+const videos = ['Radhamma Radhamma HD video Song _ Nene Raju Nene Mantri _ Rana _ Kajal Agarwal _ Anup Rubens _ Teja.mp4', 'Sukhibhava HD Full Video Song _ NRNM _ Rana Daggubatti _ Kajal Agarwal _ Anup Rubens _ Teja (1).mp4', '15921892-uhd_3840_2160_50fps (1) (1).mp4'];
 let currentVideoIndex = 0;
 
 playPauseButton.addEventListener('click', () => {
